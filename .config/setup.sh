b374b235cf0872da1015587451becc9044cb0a3f
k0rventen <corentin.farque@axians.com>
2023-03-23 21:39:43 +0100

    force checkout & verbosity

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,3 +1,6 @@
 cd $HOME
+echo "cloning dotfiles directory.."
 git clone --bare https://github.com/k0rventen/.dotfiles.git .dotfiles
-git --git-dir=.dotfiles/ --work-tree=$HOME checkout
+echo "checking out files !"
+git --git-dir=.dotfiles/ --work-tree=$HOME checkout -f
+echo "dotfiles are now installed !"
