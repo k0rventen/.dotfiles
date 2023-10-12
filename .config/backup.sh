664cb9ebb6b99bf5b7dcf5aa7f54196bb866480a
k0rventen <corentin.farque@axians.com>
2023-10-12 22:24:15 +0200

    use luks setup in backup script

--- .config/backup.sh
+++ .config/backup.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
+set -e
 
-readonly SOURCE_DIR="$HOME/services"
-readonly BACKUP_DIR="/mnt/backup/backups"
-readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
-readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
-readonly LATEST_LINK="${BACKUP_DIR}/latest"
+SOURCE_DIR="/home/me"
+BACKUP_DIR="/mnt/backup"
+DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
+BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
+LATEST_LINK="${BACKUP_DIR}/latest"
 
-mkdir -p /mnt/backup
+echo "fetching luks key from stdin"
+luks_pass=$(cat)
 
-mount /dev/sda1 /mnt/backup/
+echo "decrypting backup disk"
+echo -n "$luks_pass" | /usr/sbin/cryptsetup luksOpen /dev/sda1 backup -d -
+
+echo "mounting backup disk"
 mkdir -p "${BACKUP_DIR}"
+mount /dev/mapper/backup ${BACKUP_DIR}
 
-rsync -av --delete \
+echo "syncing backups"
+rsync --archive --delete \
   "${SOURCE_DIR}/" \
   --link-dest "${LATEST_LINK}" \
   "${BACKUP_PATH}"
@@ -23,6 +27,9 @@ rsync -av --delete \
 rm -rf "${LATEST_LINK}"
 ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
 
+echo "unmounting & closing luks"
 umount /mnt/backup
+/usr/sbin/cryptsetup luksClose backup
+
 echo "done"
 
