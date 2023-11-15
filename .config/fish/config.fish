f8f43f24fe3e257a8961e55ed5003ed0dad4d516
k0rventen <corentin@farque.net>
2023-11-16 00:09:20 +0100

    add ls colors envs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 # envs & path
 set fish_greeting 
 set -xg EDITOR nano
+set -xg LSCOLORS gxfxcxdxbxegedabagacad
+set -xg LS_COLORS 'di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 
 # very short aliases
 alias k 'kubectl'
