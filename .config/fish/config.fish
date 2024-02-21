75de7097d25e303588151a92ec3dd4631f9e5de0
k0rventen <corentin@farque.net>
2024-02-21 21:12:23 +0100

    set TERM for wider compat

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set fish_greeting
 set -xg EDITOR nano
 set -xg LSCOLORS gxfxcxdxbxegedabagacad
 set -xg LS_COLORS 'di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+set -xg TERM xterm
 
 # very short aliases
 alias k 'kubectl'
