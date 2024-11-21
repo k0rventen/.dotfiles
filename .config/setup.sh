a35d7538cf7cc6afbb8c31d6c66c20ab3ac04289
k0rventen <49311792+k0rventen@users.noreply.github.com>
2024-11-21 20:03:37 +0100

    improve setup w/ less clutter

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,6 +1,14 @@
 cd $HOME
+
 echo "cloning dotfiles directory.."
 git clone --bare https://github.com/k0rventen/.dotfiles.git .dotfiles
+
 echo "checking out files !"
 git --git-dir=.dotfiles/ --work-tree=$HOME checkout -f
+
+echo "setting ignore rules"
+echo "*" >> .dotfiles/info/exclude
+git --git-dir=.dotfiles/ --work-tree=$HOME update-index --skip-worktree README.md
+rm README.md
+
 echo "dotfiles are now installed !"
